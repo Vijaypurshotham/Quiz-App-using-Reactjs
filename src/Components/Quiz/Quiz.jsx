48c5bc6617e6c6b27af984d8bcf8321a6ade6ef0
Vijaypurshotham <vijayganeshan750@gmail.com>
2024-03-16 16:35:54 +0530

    quiz

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,136 @@
+import React, { useRef, useState } from "react";
+import "./Quiz.css";
+import { data } from "../../assets/Data";
+
+const Quiz = () => {
+  // ! Below we have set the index value to 0 which means it will start the question from 1 from data.js file
+  let [index, setIndex] = useState(0);
+  //  ? Below we have used data and passed index value i.e 0 as initial state value
+  let [question, setQuestion] = useState(data[index]);
+
+  // ~ Below we are using lock state so that only one option is selected instead of selecting multiple option
+  let [lock, setLock] = useState(false);
+
+  // ^ Below we have used score state for accessing the marks scored in an quiz
+  let [score, setScore] = useState(0);
+
+  // * Below we have used result for displaying the results of the quiz app
+  let [result, setResult] = useState(false);
+
+  // & If we click on the right answer then then wrong ans should be highlighted same vice versa for wrong
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let option_array = [Option1, Option2, Option3, Option4];
+  // ! Below function says that the answer is correct or not
+  const checkAns = (e, ans) => {
+    if (lock === false) {
+      if (question.ans === ans) {
+        e.target.classList.add("correct");
+        setLock(true);
+        setScore((prev) => prev + 1);
+      } else {
+        e.target.classList.add("wrong");
+        setLock(true);
+        option_array[question.ans - 1].current.classList.add("correct");
+      }
+    }
+  };
+
+  // ? Creating functionalities for next button
+  const next = () => {
+    if (lock === true) {
+      if (index === data.length - 1) {
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(data[index]);
+      setLock(false);
+      option_array.map((option) => {
+        option.current.classList.remove("wrong");
+        option.current.classList.remove("correct");
+        return null;
+      });
+    }
+  };
+
+  // ! Creating functionalities for reset button
+  const reset = () => {
+    setIndex(0);
+    setQuestion(data[0]);
+    setScore(0);
+    setLock(false);
+    setResult(false);
+  };
+
+  return (
+    <div className="container">
+      <h1>Quiz App</h1>
+      <hr />
+      {result ? (
+        <></>
+      ) : (
+        <>
+          {" "}
+          <h2>
+            {index + 1}. {question.question}
+          </h2>
+          <ul>
+            <li
+              ref={Option1}
+              onClick={(e) => {
+                checkAns(e, 1);
+              }}
+            >
+              {question.option1}
+            </li>
+            <li
+              ref={Option2}
+              onClick={(e) => {
+                checkAns(e, 2);
+              }}
+            >
+              {question.option2}
+            </li>
+            <li
+              ref={Option3}
+              onClick={(e) => {
+                checkAns(e, 3);
+              }}
+            >
+              {question.option3}
+            </li>
+            <li
+              ref={Option4}
+              onClick={(e) => {
+                checkAns(e, 4);
+              }}
+            >
+              {question.option4}
+            </li>
+          </ul>
+          <button onClick={next}>Next</button>
+          <div className="index">
+            {index + 1} of {data.length} questions
+          </div>
+        </>
+      )}
+      {result ? (
+        <>
+          {" "}
+          <h2>
+            You Scored {score} out of {data.length}
+          </h2>
+          <button onClick={reset}>Reset</button>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
